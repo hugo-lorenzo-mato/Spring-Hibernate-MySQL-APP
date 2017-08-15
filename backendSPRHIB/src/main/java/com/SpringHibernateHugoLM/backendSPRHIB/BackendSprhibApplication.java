fbc7252c5bdbef9c77d20656a7aa831c64e8d8e0
hlorenzo <hlorenzo@enxenio.es>
2017-08-15 12:57:36 +0200

    First commit

---
+++ backendSPRHIB/src/main/java/com/SpringHibernateHugoLM/backendSPRHIB/BackendSprhibApplication.java
@@ -0,0 +1,12 @@
+package com.SpringHibernateHugoLM.backendSPRHIB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSprhibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSprhibApplication.class, args);
+	}
+}
---
+++ backendSPRHIB/src/test/java/com/SpringHibernateHugoLM/backendSPRHIB/BackendSprhibApplicationTests.java
@@ -0,0 +1,16 @@
+package com.SpringHibernateHugoLM.backendSPRHIB;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BackendSprhibApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
